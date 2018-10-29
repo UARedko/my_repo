98b5afb6111c7d7a9499cb74f56b8af70a0c7a21
iredko <iredko@tycoint.com>
2018-10-29 15:13:40 +0200

    remove files

--- terraform/env/env-ecr/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-region            = "us-east-1"
-account_id        = ""
-account_id_prod   = ""
-tag_life_period   = 60
-consul_datacenter = "us-east-1-dev-mgmt"
--- terraform/modules/module-ecr/00-vars.tf
+++
@@ -1,7 +0,0 @@
-variable "ecr_names" { type    = "list"
-                       default = ["ecr-list"
-                                  ]
-                                }
-
-variable "account_id_prod" { default = "" }
-variable "tag_life_period" { default = 180 }
